9c1e14b75e7483abf807dcfcac6cc994b929c1a3
k0pernicus <antonin.carette@gmail.com>
2016-06-30 18:30:23 -0400

    debugging: Pep8 + tab problems

--- src/lib/network/socket_client.py
+++ src/lib/network/socket_client.py
@@ -9,11 +9,12 @@ class SocketClient(object):
         self.socket = socket(AF_INET, SOCK_STREAM)
 
     def connect(self, host, port):
-        self.socket.connect((str(host) ,int(port)))
-        print("Connection established with %s:%s" % (str(host) ,int(port)))
+        self.socket.connect((str(host), int(port)))
+        print("Connection established with %s:%s" % (str(host), int(port)))
 
     def send(self, message):
-	threading.Thread(target=self.socket.send, args=(message.encode('ASCII'),)).start()
+        threading.Thread(target=self.socket.send,
+                         args=(message.encode('ASCII'))).start()
 
     def disconnect(self):
         self.socket.close()
