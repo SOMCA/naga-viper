daa87ca67cbf70f3f5fcb28915b5cb450cde5fdc
k0pernicus <antonin.carette@gmail.com>
2016-07-07 15:19:36 -0400

    Enhancement: Add new functionalities

--- src/lib/modules/module_interface.py
+++ src/lib/modules/module_interface.py
@@ -1,7 +1,31 @@
 from abc import ABC, abstractmethod
 
+import threading
+
+
 class MODULEInterface(ABC):
-    def __init__(self):
-        pass
+
+    def __init__(self, module_name, network_usage):
+        super(MODULEInterface, self).__init__()
+        self._finished = threading.Event()
+        self._network = network_usage
+        self._values = []
+
+    @abstractmethod
+    def collecting_data(self):
+        return
+
+    def sending_data(self, data_name, data_values):
+        self._network.send_data("%s: %s" % (data_name,
+                                            ("-".join([str(value)
+                                             for value in data_values]))))
+
+    def stop(self):
+        print("[%s] DISCONNECTING...")
+        self._network.disconnect()
+        self._finished.set()
+
     def run(self):
-        pass
+        print("[%s] COLLECTING METRICS...")
+        self._network.connect()
+        threading.Thread(target=self.collecting_data).start()
